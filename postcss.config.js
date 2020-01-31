b644b1a2e6373bc330da09eecd44b3f6a4f6da8a
Julie Pierre <julie.pierre3@gmail.com>
2020-01-31 10:12:43 +0100

    configuration initiale

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-css-variables'),
+    require('postcss-custom-media'),
+    require('postcss-inline-svg'),
+    require('postcss-nested'),
+    require('postcss-url'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
