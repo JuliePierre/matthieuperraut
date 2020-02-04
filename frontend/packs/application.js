e61014760c8ffed80789ad00b2c57dc9ca2d6873
Julie Pierre <julie.pierre3@gmail.com>
2020-02-04 22:21:06 +0100

    nouvelle charte graphique ok et adaptation selon nouvelle plaquette MP

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -5,14 +5,21 @@ import 'components/page/page'
 import 'components/button/button'
 import 'components/card/card'
 import 'components/container/container'
+import 'components/feature-box/feature-box'
 import 'components/grid/grid'
 import 'components/hero/hero'
 import 'components/icon-box/icon-box'
 import 'components/section/section'
+import 'components/space/space'
+import 'components/subtitle/subtitle'
 import 'components/title/title'
 
 // MEDIA
 import 'shared/static/matthieu-perraut-mini-transat.jpg'
+import 'shared/static/skipper-matthieu-perraut.png'
+import 'shared/static/logo-mini-transat.png'
+import 'shared/static/transgascogne-matthieu-perraut.jpg'
+import 'shared/static/parcours-mini-transat-2019.png'
 
 // ICONS
 import '@fortawesome/fontawesome-free/css/all.css'
