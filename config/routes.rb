cc8b73f171f25f63590b64a798caee7fcacbca23
Julie Pierre <julie.pierre3@gmail.com>
2020-02-07 13:46:09 +0100

    ajout des redirections

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'mediatheque', to: "pages#mediatheque", as: :mediatheque
+
+  # REDIRECTIONS
+  get '/minitransat.html', to: redirect('/')
+  get '/projet.html', to: redirect('/')
+  get '/programme.html', to: redirect('/')
+  get '/bateau.html', to: redirect('/')
+  get '/partenaires.html', to: redirect('/')
+  get '/media.html', to: redirect('/')
+  get '/actualites.html', to: redirect('/')
 end
