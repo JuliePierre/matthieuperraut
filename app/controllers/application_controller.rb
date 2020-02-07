e699c9b0398abeb4c8bb8928f9aba07e1aea8b39
Julie Pierre <julie.pierre3@gmail.com>
2020-02-07 12:53:26 +0100

    ajout des meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   prepend_view_path Rails.root.join("frontend")
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
