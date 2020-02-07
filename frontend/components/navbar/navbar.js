015b92b6846965049da82c86956d4c833e52f7cd
Julie Pierre <julie.pierre3@gmail.com>
2020-02-07 13:39:04 +0100

    mini modifs

--- frontend/components/navbar/navbar.js
+++ frontend/components/navbar/navbar.js
@@ -37,33 +37,35 @@ export const manageNavbar = () => {
   const sections = Array.from(document.querySelectorAll('.js-section'))
 
   window.addEventListener('scroll', (event) => {
-    const position = banner.getBoundingClientRect()
+    if (banner) {
+      const position = banner.getBoundingClientRect()
 
-    if (position.top <= -76) {
-      navbarContainer.classList.remove('navbar-container--transparent')
-      navbarContainer.classList.add('navbar-container--blue')
-    } else if (position.top > -76) {
-      navbarContainer.classList.remove('navbar-container--blue')
-      navbarContainer.classList.add('navbar-container--transparent')
-    }
+      if (position.top <= -76) {
+        navbarContainer.classList.remove('navbar-container--transparent')
+        navbarContainer.classList.add('navbar-container--blue')
+      } else if (position.top > -76) {
+        navbarContainer.classList.remove('navbar-container--blue')
+        navbarContainer.classList.add('navbar-container--transparent')
+      }
 
-    // GET CURRENT SECTION
-    sections.forEach((section) => {
-      if (section.getBoundingClientRect().top > 0 && section.getBoundingClientRect().top <= 200) {
-        // ACTIVE SECTION
-        if (section.classList.contains('active')) {
-        } else {
-          const target = section.id
-          if (target) {
-            const link = document.querySelector(`.js-navbar__link[data-target=${target}]`)
-            clearMenu(links)
-            if (link) {
-              link.classList.add('active')
+      // GET CURRENT SECTION
+      sections.forEach((section) => {
+        if (section.getBoundingClientRect().top > 0 && section.getBoundingClientRect().top <= 200) {
+          // ACTIVE SECTION
+          if (section.classList.contains('active')) {
+          } else {
+            const target = section.id
+            if (target) {
+              const link = document.querySelector(`.js-navbar__link[data-target=${target}]`)
+              clearMenu(links)
+              if (link) {
+                link.classList.add('active')
+              }
             }
           }
         }
-      }
-    })
+      })
+    }
   })
 
   manageMobileSidebar()
